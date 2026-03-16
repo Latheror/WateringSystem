60abbb847aa28a2ba3d2ccf19130d30278aa252a
Romain Jegat <lathgames@gmail.com>
2026-03-16 19:40:07 +0100

    Write demo code to blink led

--- system/system.ino
+++ system/system.ino
@@ -1,9 +1,13 @@
-void setup() {
-  // put your setup code here, to run once:
+// Define the LED pin for ESP32C3
+#define LED_BUILTIN 8
 
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on
+  delay(100);                      // wait 100 ms
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off
+  delay(100);                      // wait 100 ms
 }
