62fd16a725a21ca6ec1ee986f0db8364254e19e8
Romain Jegat <lathgames@gmail.com>
2026-06-07 12:21:15 +0200

    Read moisture sensor value. Move wifi credentials into a new "user settings" file.

--- system/settings.h
+++ system/settings.h
@@ -6,18 +6,17 @@
  * @brief Project-wide configuration constants for Watering System.
  */
 
-/**
- * @brief WiFi SSID for network connection.
- */
-#define WIFI_SSID "your_ssid"
-/**
- * @brief WiFi password for network connection.
- */
-#define WIFI_PASS "your_password"
+#include "settings_user.h"
 
 /**
  * @brief GPIO pin number for the onboard LED (ESP32C3).
  */
 #define LED_BUILTIN 8
+
+// Soil moisture sensor pins
+// Analog pin connected to sensor analog output (AO)
+#define SOIL_MOISTURE_SENSOR_ANALOG_PIN 0
+// Digital pin connected to sensor digital output (DO)
+#define SOIL_MOISTURE_SENSOR_DIGITAL_PIN 1
 
 #endif // SETTINGS_H
